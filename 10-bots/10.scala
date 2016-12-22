5aec35dceaad0fa3b4509e36ae6f76b82c2bc530
Matěj Korvas <matej.korvas@nuance.com>
2016-12-22 21:43:38 +0100

    Revert "Work on a partial information version of 10-bots"
    This reverts commit 892ed686d1f6daba4cfcc65dc884fda5bfcc2a57.

--- 10-bots/10.scala
+++ 10-bots/10.scala
@@ -1,7 +1,6 @@
 #!/usr/bin/scala
-# vim: set ft=scala et ts=2 sw=2 :
 
-abstract class ChipHolder(val id: Int)
+abstract class ChipHolder(id: Int)
 object ChipHolder {
   def parse(typeStr: String, numStr: String): ChipHolder =
     typeStr match {
@@ -20,161 +19,30 @@ object OutputBin {
   def apply(id: Int) = id2bin.getOrElseUpdate(id, new OutputBin(id))
 }
 
-class Interval(val min: Option[Int], val max: Option[Int]) extends Comparable[Interval] {
-  def compareTo(other: Interval) = {
-    if (max.isDefined && other.min.isDefined && max.get < other.min.get) -1 else
-    if (min.isDefined && other.max.isDefined && min.get > other.max.get)  1 else
-                                                                  0
-  }
-}
-
-// val ivals = Array(new Interval(None, None),
-//                   new Interval(Some(41), None))
-// 
-// ivals.map(_.min).max.map(_ + 1)
-// ivals.map(_.max).min.map(_ - 1)
-
-class Chip extends Comparable[Chip] {
-  private var _idRange = new Interval(None, None)
-  private var _value: Option[Int] = None
-  var lowerChips = Set[Chip]()
-  var higherChips = Set[Chip]()
-  
-  def this(id: Int) {
-    this()
-    value = id
-  }
-
-  def this(id: Option[Int]) {
-    this()
-    if (id.isDefined)
-      value = id.get
-  }
-
-  // Mark this chip as lower than another chip.
-  def :<(higher: Chip) {
-    higherChips += higher
-    higher.lowerChips += this
-  }
-
-  def compareTo(other: Chip) = idRange.compareTo(other.idRange)
-
-  def value = {
-    if (!_value.isDefined)
-      idRange()
-    _value
-  }
-
-  def value_=(knownValue: Int) {
-    if (!_value.isDefined) {
-      Chip.id2chips(knownValue) += this
-      _value = Some(knownValue)
-      _idRange = new Interval(_value, _value)
-    }
-  }
-
-  def idRange(): Interval = {
-    if (!_value.isDefined) {
-      val knownMin = if (lowerChips.nonEmpty) 
-                        lowerChips.map(_._idRange.min).max.map(_ + 1)
-                     else None
-      val knownMax = if (higherChips.nonEmpty)
-                        higherChips.map(_._idRange.max).min.map(_ - 1)
-                     else None
-      _idRange = new Interval(knownMin, knownMax)
-      if (knownMin.isDefined && knownMin == knownMax)
-        value = knownMin.get
-    }
-    _idRange
-  }
-}
-object Chip {
-  private val id2chips = scala.collection.mutable.Map[Int, Set[Chip]]()
-                         .withDefaultValue(Set[Chip]())
-  def apply(u: Unit) = new Chip()
-  def apply(id: Int) = {
-    id2chips.get(id) match {
-      case chips: Some[Set[Chip]] => chips.get.head
-      case None => new Chip(id)
-    }
-  }
-}
-
 object BotFactory {
 
-  val holder2chips = scala.collection.mutable.Map[ChipHolder, List[Set[Chip]]]()
-                     .withDefaultValue(Nil)
-                     // .withDefaultValue(List[Set[Chip]]())
-  val chip2bot = scala.collection.mutable.Map[Chip, Bot]()
-  val chip2bots = scala.collection.mutable.Map[Chip, Set[Bot]]()
-                     .withDefaultValue(Set[Bot]())
+  val holder2chips = scala.collection.mutable.Map[ChipHolder, List[Int]]()
+                     .withDefaultValue(List[Int]())
+  val chip2bot = scala.collection.mutable.Map[Int, Bot]()
 
-//   def moveChip(chip: Chip, whom: ChipHolder) {
-//     chip2bot.get(chip).foreach( oldBot =>
-//           holder2chips(oldBot) = holder2chips(oldBot).filter(_ != chip))
-//     holder2chips(whom) = chip :: holder2chips(whom)
-//     whom match {
-//       case bot: Bot => chip2bot(chip) = bot
-//       case _ => ()
-//     }
-//   }
-
-  def copyChipset(chipset: Set[Chip], whom: ChipHolder) {
-    holder2chips(whom) = chipset :: holder2chips(whom)
-    if (chipset.size == 1) {
-      whom match {
-        case bot: Bot => chip2bot(chipset.head) = bot
-        case _ => ()
-      }
-    }
-  }
-
-  def moveChipset(chipset: Set[Chip], whom: ChipHolder) {
-    chipset.foreach( chip =>
-      chip2bots(chip).foreach( holder =>
-        holder2chips(holder) = holder2chips(holder)
-          .map(_ - chip).filter(_.nonEmpty)
-    ))
-    copyChipset(chipset, whom)
-  }
-
-//   def giveChips(botSrc: Bot, outLow: ChipHolder, outHigh: ChipHolder) {
-//     val List(chip1, chip2) = holder2chips(botSrc)
-//     val (low, high) = if (chip1 < chip2) (chip1, chip2) else (chip2, chip1)
-//     moveChip(low, outLow)
-//     moveChip(high, outHigh)
-//   }
-
-  def fillInTwoChipsets(knownChipsets: List[Set[Chip]]) = {
-    knownChipsets match {
-      case List(first, second) => knownChipsets
-      case List(first) => List(first, Set(Chip()))
-      case Nil => List(Set(Chip()), Set(Chip()))
+  def moveChip(chip: Int, whom: ChipHolder) {
+    chip2bot.get(chip).foreach( oldBot =>
+          holder2chips(oldBot) = holder2chips(oldBot).filter(_ != chip))
+    holder2chips(whom) = chip :: holder2chips(whom)
+    whom match {
+      case bot: Bot => chip2bot(chip) = bot
+      case _ => ()
     }
   }
 
   def giveChips(botSrc: Bot, outLow: ChipHolder, outHigh: ChipHolder) {
-    val List(chipset1, chipset2) = fillInTwoChipsets(holder2chips(botSrc))
-    val comparisons =
-      for (chip1 <- chipset1; chip2 <- chipset2)
-        yield chip1.compareTo(chip2)
-    if (comparisons.forall(_ < 0)) {
-      moveChipset(chipset1, outLow)
-      moveChipset(chipset2, outHigh)
-    }
-    else if (comparisons.forall(_ > 0)) {
-      moveChipset(chipset2, outLow)
-      moveChipset(chipset1, outHigh)
-    }
-    else {
-      moveChipset(chipset1 | chipset2, outLow)
-      copyChipset(chipset1 | chipset2, outHigh)
-    }
+    val List(chip1, chip2) = holder2chips(botSrc)
+    val (low, high) = if (chip1 < chip2) (chip1, chip2) else (chip2, chip1)
+    moveChip(low, outLow)
+    moveChip(high, outHigh)
   }
 
 }
-
-// for (i <- (1 to 5)) yield i + 1
 
 // BotFactory.moveChip(23, Bot(208))
 // BotFactory.moveChip(24, Bot(209))
@@ -196,7 +64,7 @@ for (line <- Source.fromFile("in", "UTF-8").getLines) {
 // for (line <- inLines) {
   line match {
     case movePat(chip, bot) =>
-      BotFactory.moveChipset(Set(Chip(chip.toInt)), Bot(bot.toInt))
+      BotFactory.moveChip(chip.toInt, Bot(bot.toInt))
     case givePat(from, lowType, toLow, highType, toHigh) =>
       BotFactory.giveChips(Bot(from.toInt),
                            ChipHolder.parse(lowType, toLow),
@@ -204,8 +72,3 @@ for (line <- Source.fromFile("in", "UTF-8").getLines) {
     case _ => println(s"no match: $line")
   }
 }
-
-val hid2mins = for ((k, v) <- BotFactory.holder2chips)
-  yield (k.id, (for (cs <- v)
-                  yield (for (c <- cs)
-                          yield c.idRange.min)))
