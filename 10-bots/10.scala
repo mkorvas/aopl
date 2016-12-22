8d8c93f9a6012c7b5f139257a992ce5119509b53
Matěj Korvas <matej.korvas@nuance.com>
2016-12-22 22:33:45 +0100

    Solve 10-bots (Scala)

--- 10-bots/10.scala
+++ 10-bots/10.scala
@@ -1,6 +1,9 @@
 #!/usr/bin/scala
+# vim: set ft=scala et ts=2 sw=2 :
 
-abstract class ChipHolder(id: Int)
+abstract class ChipHolder(val id: Int) {
+  def receive(chip: Int)
+}
 object ChipHolder {
   def parse(typeStr: String, numStr: String): ChipHolder =
     typeStr match {
@@ -8,67 +11,59 @@ object ChipHolder {
       case "output" => OutputBin(numStr.toInt)
     }
 }
-class Bot(id: Int) extends ChipHolder(id)
+class Bot(id: Int) extends ChipHolder(id) {
+  var chips: List[Int] = Nil
+  var instructions: List[Tuple2[ChipHolder, ChipHolder]] = Nil
+  override def receive(chip: Int) {
+    chips = chips match {
+      case Nil                           => List(chip)
+      case List(larger) if larger > chip => List(chip, larger)
+      case List(smaller)                 => List(smaller, chip)
+    }
+    if (chips == List(17, 61))
+      println(s"Bot ${id} will compare 17 and 61.")
+    if (chips.length == 2 && instructions.nonEmpty) doDistribute
+  }
+  def distribute(outLow: ChipHolder, outHigh: ChipHolder) {
+    instructions :+= (outLow, outHigh)
+    if (chips.length == 2) doDistribute
+  }
+  def doDistribute() {
+    val ((outLow, outHigh) :: rest) = instructions
+    instructions = rest
+    outLow.receive(chips(0))
+    outHigh.receive(chips(1))
+    chips = Nil
+  }
+}
 object Bot {
   private val id2bot = scala.collection.mutable.Map[Int, Bot]()
   def apply(id: Int) = id2bot.getOrElseUpdate(id, new Bot(id))
 }
-class OutputBin(id: Int) extends ChipHolder(id)
+class OutputBin(id: Int) extends ChipHolder(id) {
+  var chips: List[Int] = Nil
+  def receive(chip: Int) { chips :+= chip }
+}
 object OutputBin {
   private val id2bin = scala.collection.mutable.Map[Int, OutputBin]()
   def apply(id: Int) = id2bin.getOrElseUpdate(id, new OutputBin(id))
+  def outputMap = id2bin
 }
-
-object BotFactory {
-
-  val holder2chips = scala.collection.mutable.Map[ChipHolder, List[Int]]()
-                     .withDefaultValue(List[Int]())
-  val chip2bot = scala.collection.mutable.Map[Int, Bot]()
-
-  def moveChip(chip: Int, whom: ChipHolder) {
-    chip2bot.get(chip).foreach( oldBot =>
-          holder2chips(oldBot) = holder2chips(oldBot).filter(_ != chip))
-    holder2chips(whom) = chip :: holder2chips(whom)
-    whom match {
-      case bot: Bot => chip2bot(chip) = bot
-      case _ => ()
-    }
-  }
-
-  def giveChips(botSrc: Bot, outLow: ChipHolder, outHigh: ChipHolder) {
-    val List(chip1, chip2) = holder2chips(botSrc)
-    val (low, high) = if (chip1 < chip2) (chip1, chip2) else (chip2, chip1)
-    moveChip(low, outLow)
-    moveChip(high, outHigh)
-  }
-
-}
-
-// BotFactory.moveChip(23, Bot(208))
-// BotFactory.moveChip(24, Bot(209))
-// BotFactory.moveChip(23, Bot(209))
-// BotFactory.giveChips(Bot(209), OutputBin(0), Bot(208))
-
-// BotFactory.holder2chips
-
-val inLines = Array(
-  "value 23 goes to bot 208",
-  "bot 125 gives low to bot 58 and high to bot 57"
-  )
 
 val movePat = """value ([0-9]+) .* ([0-9]+)""".r
 val givePat = """bot ([0-9]+) .* (bot|output) ([0-9]+) .* (bot|output) ([0-9]+)""".r
 
 import scala.io.Source
+
 for (line <- Source.fromFile("in", "UTF-8").getLines) {
-// for (line <- inLines) {
   line match {
-    case movePat(chip, bot) =>
-      BotFactory.moveChip(chip.toInt, Bot(bot.toInt))
+    case movePat(chip, bot) => Bot(bot.toInt).receive(chip.toInt)
     case givePat(from, lowType, toLow, highType, toHigh) =>
-      BotFactory.giveChips(Bot(from.toInt),
-                           ChipHolder.parse(lowType, toLow),
-                           ChipHolder.parse(highType, toHigh))
+      Bot(from.toInt).distribute(ChipHolder.parse(lowType, toLow),
+                                 ChipHolder.parse(highType, toHigh))
     case _ => println(s"no match: $line")
   }
 }
+
+OutputBin.outputMap.filterKeys(Set(0, 1, 2))
+  .values.map(_.chips.last).product
