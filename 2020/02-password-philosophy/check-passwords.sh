48622c8d90db68413f28b19de04ba44202358e4b
Matěj Korvas <matej.korvas@cerence.com>
2020-12-02 09:38:26 +0100

    Password Philosophy, part 2

--- 2020/02-password-philosophy/check-passwords.sh
+++ 2020/02-password-philosophy/check-passwords.sh
@@ -8,8 +8,18 @@ check_line() {
   [[ $min -le ${#ltrs_in_password} && $max -ge ${#ltrs_in_password} ]]
 }
 
+check_line2() {
+  read freq_range ltr password
+  ltr=${ltr%:}
+  IFS=- read pos1 pos2 <<<$freq_range
+  pw_ltr1=${password:$(($pos1-1)):1}
+  pw_ltr2=${password:$(($pos2-1)):1}
+  [[ $pw_ltr1 == $ltr && $pw_ltr2 != $ltr ||
+     $pw_ltr2 == $ltr && $pw_ltr1 != $ltr ]]
+}
+
 num_good=0
 while read line; do
-  check_line <<<"$line" && let num_good+=1
+  check_line2 <<<"$line" && let num_good+=1
 done
 echo $num_good
